f429198fce7df579b7eab81385447cbfaec9fb9d
Justin <justisl9@uci.edu>
2023-04-25 21:11:55 -0700

    Now counts subdomain frequency and moved, modified is_valid

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ from urllib.parse import urlparse, urljoin
 
 
 class Scraper:
-    allowed_domains = {"www.ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"}
     discovered_urls = set()
+    subdomain_frequency = dict()
     longest_page = ('', 0) # (page name, length)
     common_words = dict() # {word:times seen}
     with open("stopwords.txt") as sw_file:
@@ -15,27 +15,26 @@ class Scraper:
         # Decide whether to crawl this url or not. 
         # If you decide to crawl it, return True; otherwise return False.
         # There are already some conditions that return False.
-        try:
-            if url not in Scraper.discovered_urls and url:
-                try:
-                    parsed = urlparse(url)
-                    if parsed.scheme not in set(["http", "https"]):
-                        return False
-                    return not re.match(
-                        r".*\.(css|js|bmp|gif|jpe?g|ico"
-                        + r"|png|tiff?|mid|mp2|mp3|mp4"
-                        + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-                        + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-                        + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-                        + r"|epub|dll|cnf|tgz|sha1"
-                        + r"|thmx|mso|arff|rtf|jar|csv"
-                        + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        if url not in Scraper.discovered_urls and url:
+            try:
+                parsed = urlparse(url)
+                if parsed.scheme not in set(["http", "https"]):
+                    return False
+                return not re.match(
+                    r".*\.(css|js|bmp|gif|jpe?g|ico"
+                    + r"|png|tiff?|mid|mp2|mp3|mp4"
+                    + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                    + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                    + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                    + r"|epub|dll|cnf|tgz|sha1"
+                    + r"|thmx|mso|arff|rtf|jar|csv"
+                    + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()) \
+                    and re.match(r"^(www\.).*(ics|cs|informatics|stat)\.uci\.edu$", parsed.netloc)
 
-                except TypeError:
-                    print ("TypeError for ", parsed)
-                    raise
-        except Exception as e:
-            print(e)
+            except TypeError:
+                print ("TypeError for ", parsed)
+                raise
+        
             
     def __init__(self):
         pass
@@ -62,16 +61,22 @@ class Scraper:
         # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
         ret_link = []
         if resp and resp.status == 200:
+            if (resp.url.find("ics.uci.edu") != -1 and not resp.url.find("informatics.uci.edu") != -1) and not re.match(r"^.*(www\.)(ics)\.uci\.edu.*", resp.url):
+                try:
+                    Scraper.subdomain_frequency[resp.url] += 1
+                except KeyError:
+                    Scraper.subdomain_frequency[resp.url] = 1
             soup = BeautifulSoup(resp.raw_response.text, 'html.parser') # soup object
             words = re.findall("[a-zA-Z0-9]+", soup.get_text().strip().lower()) # fetches all alphanumeric tokens
             Scraper.count_words(words, resp.url)
             for link in soup.find_all('a', href=True):
                 new_link = urljoin(resp.url, link.get('href'), allow_fragments=False)
-                if re.match(r"^(www\.).*(ics|cs|informatics|stat)\.uci\.edu$", urlparse(new_link).netloc):
-                    pound_ind = new_link.find('#')
-                    if pound_ind != -1:
-                        new_link = new_link[:pound_ind]
-                    ret_link.append(new_link)
+                pound_ind = new_link.find('#')
+                if pound_ind != -1:
+                    new_link = new_link[:pound_ind]
+                ret_link.append(new_link)
+        print(Scraper.longest_page)
+        print('subdomain',Scraper.subdomain_frequency)
         return ret_link
 
     def count_words(words : list[str], url : str) -> None:
